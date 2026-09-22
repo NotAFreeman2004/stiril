a47a522d1577b995c156af0780bd5bec2924d84a
NotAFreeman2004 <volodya.batalin@bk.ru>
2026-09-22 18:54:45 +0500

    Update popo.py

--- popo.py
+++ popo.py
@@ -1,17 +1,1 @@
-x = "abcdefgh"
-x2 = "12345678"
-
-class Square:
-    def __init__(self, name, occupied):
-        self.name = name
-    def is_occupied(self, occupied):
-        if 
-class Figure:
-    def __init__(self, pos):
-        self.pos = pos
-    def move(self, pos):
-        if 
-    def attack(self, pos):
-
-game = True
-while game:
+nononoon don't look here' 
